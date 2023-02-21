c8c6dc742cc2b6e4effbd7c0180bd7b469456684
Lastprismer <lastprismer@stu.pku.edu.cn>
2023-02-21 19:26:21 +0800

    1104更新

--- CatalystZh.cs
+++ CatalystZh.cs
@@ -15,41 +15,11 @@ namespace CatalystZh
         // From LanguageFix mod and Improve Game
         public override void Load()
         {
-            SetLanguage_GameCulture += Fix;
             GetFriendlyName += TranslatedFriendlyName;
-            Main.QueueMainThreadAction(() =>
-            {
-                Thread.CurrentThread.CurrentCulture = new CultureInfo("en-US");
-            });
-            if (ModLoader.TryGetMod("CatalystMod", out Mod Catalyst))
-            {
-                if(Catalyst.TryFind("AstrageldonRelic",out ModItem AstrageldonRelic))
-                {
-                    AstrageldonRelic.DisplayName.SetDefault("末世星史莱姆圣物");
-                }
-            }
         }
         public override void Unload()
         {
-            Main.QueueMainThreadAction(() =>
-            {
-                Thread.CurrentThread.CurrentCulture = Language.ActiveCulture.CultureInfo;
-            });
-            if (ModLoader.TryGetMod("CatalystMod", out Mod Catalyst))
-            {
-                if (Catalyst.TryFind("AstrageldonRelic", out ModItem AstrageldonRelic))
-                {
-                    AstrageldonRelic.DisplayName.SetDefault("Astrageldon Relic");
-                }
-            }
-            SetLanguage_GameCulture -= Fix;
             GetFriendlyName -= TranslatedFriendlyName;
-        }
-
-        private void Fix(orig_SetLanguage_GameCulture orig, LanguageManager self, GameCulture culture)
-        {
-            orig.Invoke(self, culture);
-            Thread.CurrentThread.CurrentCulture = new CultureInfo("en-US");
         }
         private string TranslatedFriendlyName(orig_GetFriendlyName orig, UIKeybindingListItem item)
         {
@@ -57,6 +27,9 @@ namespace CatalystZh
             if (Language.ActiveCulture == GameCulture.FromCultureName(GameCulture.CultureName.Chinese) && keybindName == "CatalystMod: Influx Cluster")
             {
                 return "灾劫：凝涌星核冲刺";
+            }
+            else if (Language.ActiveCulture == GameCulture.FromCultureName(GameCulture.CultureName.Chinese) && keybindName == "CatalystMod: Toggle Intergelactic Setbonus Visibility") {
+                return "灾劫：开关异宇星凝套星环可见性";
             }
             return orig.Invoke(item);
         }
